640231e8ded9bb44cf2c3554d6654fe271a590d5
mirigerzon <A0583286530@gmail.com>
2025-12-10 22:43:42 +0200

    final

---
+++ Ex03_Q1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Ex03_Q1
+{
+    public delegate double BinaryOperation(double x, double y);
+
+    public class Operations
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double ApplyOperation(BinaryOperation bOp, double a, double b)
+        {
+            return bOp(a, b);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Ex03_Q2_5/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace Ex03_Q2_5;
+
+class Program
+{
+    public class ArrayProcessor
+    {
+        public delegate void UnaryAction(double a);
+
+        public static void ProcessArray(double[] array, UnaryAction processor)
+        {
+            foreach (double item in array)
+            {
+                processor(item);
+            }
+        }
+    }
+
+    //חישוב סכום
+    public class SumCalculator
+    {
+        public double Sum { get; private set; } = 0;
+
+        public void AddToSum(double number)
+        {
+            Sum += number;
+        }
+    }
+    //חישוב מקסימום
+    public class MaxCalculator
+    {
+        public double Max { get; private set; } = double.NegativeInfinity;
+
+        public void CalculateIfMax(double number)
+        {
+            if (number > Max)
+                Max = number;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Random rndNum = new Random();
+        double[] arr = new double[10];
+        for (int i = 0; i < arr.Length; i++)
+            arr[i] = rndNum.NextDouble() * 100;
+
+        SumCalculator s = new SumCalculator();
+        ArrayProcessor.ProcessArray(arr, s.AddToSum);
+        Console.WriteLine("Sum = " + s.Sum);
+
+        MaxCalculator m = new MaxCalculator();
+        ArrayProcessor.ProcessArray(arr, m.CalculateIfMax);
+        Console.WriteLine("Max = " + m.Max);
+
+        double lambdaSum = 0;
+        ArrayProcessor.ProcessArray(arr, x => lambdaSum += x);
+        Console.WriteLine("Lambda sum = " + lambdaSum);
+
+        double lambdaMax = double.NegativeInfinity;
+        ArrayProcessor.ProcessArray(arr, x =>
+        {
+            if (x > lambdaMax)
+                lambdaMax = x;
+        });
+        Console.WriteLine("Lambda max = " + lambdaMax);
+    }
+}
